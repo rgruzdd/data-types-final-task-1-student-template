5d18e39f178588301cb4201ce0c06a40a35e7cdf
Дмитрий Колб <dm.kolb@gmail.com>
2022-08-29 07:21:14 +0000

    Update tasks/task.py

--- tasks/task.py
+++ tasks/task.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import Any, List, Str
 
-def check(lst: List):
+def check(lst: List[Dict[Any, Any]]) -> Set[str]:
     """
     Add your code here or call it from here   
     """
