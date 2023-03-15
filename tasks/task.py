edffcc72b657cb89f5f6e2c33cb879c706eaf352
Gruzin <gruzd.roman@inbox.ru>
2023-03-15 23:00:04 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -4,4 +4,14 @@ def check(lst: List[Dict[Any, Any]]) -> Set[Any]:
     """
     Add your code here or call it from here   
     """
+    new_list = []
+    for i in lst:
+        for value in i.values():
+            if value not in new_list:
+                new_list.append(value)
+
+
+    return new_list
+
     pass
+
